0f88b3e5603f8022ef11c543a20037510395b135
Edgar Lopez <edgarlopezmgs@live.com>
2021-03-21 18:44:48 -0600

    change of url

--- nuxt.config.js
+++ nuxt.config.js
@@ -49,7 +49,7 @@ export default {
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    baseURL: 'https://user-roles-4r1b8ejus-edglo98.vercel.app',
+    baseURL: 'https://user-roles.vercel.app',
     proxyHeaders: false,
     credentials: false
   },
